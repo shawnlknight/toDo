9f03cb8af584fe4aabd31483332bbf4b34b22a65
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-30 20:28:16 -0400

    added some functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,16 +6,10 @@ $(document).ready(function() {
 	// $(".taskList").append(myTasks);
 
 
-	$(".taskList").on("click", ".removeTask", function() {
-		$(this).closest("ul").remove();
-	});
 
+// This keeps track of current tasks
+$(".itemsLeft").html(tasks.length+" items left");
 
-	$(".jumbotron").on("click", "button", function() {
-
-		$('#myModal').modal();
-
-	});
 
 	$("#newTaskForm").on("submit", function(e) {
 		e.preventDefault();
@@ -26,13 +20,67 @@ $(document).ready(function() {
 			title: taskItem
 		};
 
-		tasks.unshift(newTaskObj);
+		tasks.push(newTaskObj);
 		var myTasks = _.template($("#tasksTmpl").html(), tasks);
 			$(".taskList").html(myTasks);
 
 
 		$(".newTaskItem").val("");
+
+		// This keeps track of current tasks
+		$(".itemsLeft").html(tasks.length+" items left");
 	});
+
+
+	// This removes task from list
+	// $(".taskList").on("click", ".toggleTask", function() {
+	// 	$(this).closest("li").remove();
+	// });
+
+
+	//Tooggle class lineThrough with this function. Event delegation.
+	$(".taskList").on("click", ".toggleTask", function() {
+		console.log("working");
+		$(this).closest("li").toggleClass("lineThrough");
+		var currentTasks = $(".taskList li").not(".lineThrough");
+		console.log(currentTasks);
+
+		// This keeps track of current tasks
+		$(".itemsLeft").html(currentTasks.length+" items left");
+	});
+
+
+	//Double-click to edit task
+	var originalTask;
+	$(".taskList").on('dblclick', 'li', function () {
+		var thisIndex = $(this).closest("li").data("index");
+		console.log(thisIndex);
+
+	    originalTask = $(this).text();
+
+	    tasks.splice(thisIndex, 1);
+	    $(this).text("");
+	    $("<input type='text'>").appendTo(this).focus();
+
+	    // This keeps track of current tasks
+	    $(".itemsLeft").html(tasks.length+" items left");
+	});
+	$(".taskList").on('focusout', 'li > input', function () {
+
+	    var $this = $(this);
+
+	    var newTaskObj = {
+			title: $this.val() || originalTask
+		};
+
+		tasks.unshift(newTaskObj);
+		var myTasks = _.template($("#tasksTmpl").html(), tasks);
+			$(".taskList").html(myTasks);
+			
+        // This keeps track of current tasks
+		$(".itemsLeft").html(tasks.length+" items left");
+	});
+
 
 
 
